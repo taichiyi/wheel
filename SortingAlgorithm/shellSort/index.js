863436c629f31a00f98140522978c5454b66039a
taichiyi <taichiyi@foxmail.com>
2021-01-23 23:04:06 +0800

    style: eslint

--- SortingAlgorithm/shellSort/index.js
+++ SortingAlgorithm/shellSort/index.js
@@ -1,14 +1,18 @@
 'use strict';
 
 function shellSort(arr) {
-  for (let gap = Math.floor(arr.length / 2); gap > 0; gap = Math.floor(gap / 2)) {
+  for (
+    let gap = Math.floor(arr.length / 2);
+    gap > 0;
+    gap = Math.floor(gap / 2)
+  ) {
     for (let i = gap; i < arr.length; i++) {
       for (let j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap) {
-        [arr[j], arr[j + gap]] = [arr[j + gap], arr[j]]
+        [arr[j], arr[j + gap]] = [arr[j + gap], arr[j]];
       }
     }
   }
-  return arr
+  return arr;
 
   // 低效
   // for (
