cc2286e68fd5b7d915d2d538a71a7db9b04a1a48
taichiyi <taichiyi@163.com>
2021-10-11 09:03:13 +0800

    refactor: 重构快排,让其更好理解

--- SortingAlgorithm/quickSort/demo.js
+++ SortingAlgorithm/quickSort/demo.js
@@ -1,27 +1,51 @@
 'use strict';
 
-function quickSort(arr) {
-  function partition(start, end) {
-    const pivot = arr[end];
-    let pivotIndex = start - 1;
-    for (let i = start; i < end; i++) {
+function quickSort1(arr) {
+  function partition(left, right) {
+    const pivot = arr[right];
+    let pivotIndex = left - 1;
+    for (let i = left; i < right; i++) {
       if (arr[i] < pivot) {
         pivotIndex++;
         [arr[pivotIndex], arr[i]] = [arr[i], arr[pivotIndex]];
       }
     }
     pivotIndex++;
-    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+    [arr[pivotIndex], arr[right]] = [arr[right], arr[pivotIndex]];
     return pivotIndex;
   }
-  function sort(start, end) {
-    if (end - start <= 0) return;
-    const pivotIndex = partition(start, end);
-    sort(start, pivotIndex - 1);
-    sort(pivotIndex + 1, end);
+  function sort(left, right) {
+    if (right - left <= 0) return;
+    const pivotIndex = partition(left, right);
+    sort(left, pivotIndex - 1);
+    sort(pivotIndex + 1, right);
   }
   sort(0, arr.length - 1);
   return arr;
 }
 
+function quickSort(arr) {
+  const getPivotIndex = (left, right) => {
+    const pivot = arr[right]
+    let pivotIndex = left - 1
+    for (let i = left; i <= right; i++) {
+      if (arr[i] <= pivot) {
+        pivotIndex++;
+        if (pivotIndex !== i) {
+          [arr[pivotIndex], arr[i]] = [arr[i], arr[pivotIndex]]
+        }
+      }
+    }
+    return pivotIndex
+  }
+  const partition = (left, right) => {
+    if (right - left > 0) {
+      const pivotIndex = getPivotIndex(left, right)
+      partition(left, pivotIndex - 1)
+      partition(pivotIndex + 1, right)
+    }
+  }
+  partition(0, arr.length - 1)
+  return arr;
+}
 module.exports = quickSort;
--- SortingAlgorithm/quickSort/index.js
+++ SortingAlgorithm/quickSort/index.js
@@ -1,13 +1,46 @@
 'use strict';
-
-module.exports=function quickSort(arr) {
-  function sort(start,end){
-    if(end-start<=0)return
-    const pivotIndex = partition(start,end)
-    sort(start,pivotIndex-1)
-    sort(pivotIndex,end)
+function quickSort(arr) {
+  const getPivotIndex = (left, right) => {
+    const pivot = arr[right]
+    let pivotIndex = left - 1
+    for (let i = left; i <= right; i++) {
+      if (arr[i] <= pivot) {
+        pivotIndex++;
+        if (pivotIndex !== i) {
+          [arr[pivotIndex], arr[i]] = [arr[i], arr[pivotIndex]]
+        }
+      }
+    }
+    return pivotIndex
   }
-  sort(0,arr.length-1)
-  return arr
+  const partition = (left, right) => {
+    if (right - left > 0) {
+      const pivotIndex = getPivotIndex(left, right)
+      partition(left, pivotIndex - 1)
+      partition(pivotIndex + 1, right)
+    }
+  }
+  partition(0, arr.length - 1)
+  return arr;
 }
-
+var quickSort1 = function(nums) {
+  function sort(nums, l, r) {
+    if (l >= r) {
+      return nums;
+    }
+    let i = l - 1, j = r + 1;
+    const mid = nums[(l + r) >> 1];
+    while (i < j) {
+      while (nums[++i] < mid) {};
+      while (nums[--j] > mid) {};
+      if (i < j) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+    sort(nums, l, j);
+    sort(nums, j + 1, r);
+    return nums;
+  }
+  return sort(nums, 0, nums.length - 1);
+}
+module.exports = quickSort
